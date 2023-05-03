95eb9af316bec47c802e18a8ef35098546ba71e2
philmui <thephilmui@gmail.com>
2023-05-03 08:25:16 -0700

    adding logic to LLM site

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Global Commerce with LLM
+
+@philmui
+Wed May  3 08:23:15 PDT 2023
---
+++ main.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+prompt_template = """
+Please answer this question succinctly and professionally:
+{query}
+"""
+
+prompt = PromptTemplate(
+    input_variables=["query"],
+    template=prompt_template
+)
+
+def load_LLM():
+    llm = OpenAI(model_name=option_llm, temperature=0)
+    return llm
+
+
+##############################################################################
+
+st.set_page_config(page_title="Global Commerce", page_icon=":robot:")
+st.header("Global Commerce")
+
+col1, col2 = st.columns([1,2])
+
+with col1:
+    option_llm = st.selectbox(
+        "Which LLM would you like to use?",
+        ('text-davinci-003', 
+         'text-babbage-001', 
+         'text-ada-001',
+         'cohere',
+         'dolly')
+    )
+with col2:
+    pass
+
+def get_question():
+    input_text = st.text_area(label="Your question ...", 
+                              placeholder="Ask me here.",
+                              key="question_text")
+    return input_text
+
+question_text = get_question()
+if question_text:
+    st.markdown(f"_chosen llm_: {option_llm}")
+    prompt_formatted = prompt.format(query=question_text)
+    # st.write(prompt_formatted)
+    llm = load_LLM()
+    response = llm(prompt_formatted)
+    st.write(response)
+
+##############################################################################
+
+col1, col2 = st.columns([1,2])
+
+with col1:
+    st.markdown("#### 3 types of reasoning:")
+    st.markdown("* LLM and common sense reasoning")
+    st.markdown("* local ('secure') data analysis")
+    st.markdown("* 3rd party enhanced reasoning")
+
+with col2:
+    st.image(image="plugins.png", width=500, caption="salesforce.com")
+
+##############################################################################
